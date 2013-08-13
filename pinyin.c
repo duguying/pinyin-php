c2aa69bd097b1487aabdfd2f6cb5c07d348b83d4
rex lee <duguying2008@gmail.com>
2013-08-13 19:53:37 +0800

    fn:cutCnChar get the ith chinese character

--- pinyin.c
+++ pinyin.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
 #include "pinyin.inc"
+
+/**
+ * The struct of a single chinese character
+ */
+typedef struct character{
+	char cnch[2];//a single character
+	char* piny;//the pinyin char of the character
+	char* pyab;//the pinyin alphabet of the character
+	struct character* next;//point to the next character struct
+} cchar;
+/**
+ * cut the chinese character
+ * 2013-8-13
+ * @param cncharlist the chinese character list
+ * @param i the ith chinese character
+ * @return the chinese character
+ */
+char* cutCnChar(char* cncharlist,int i){
+	int length=sizeof(cncharlist)/2;
+	int start;
+	start=i*2;
+	static char ch[3];//keep the var as static
+	ch[0]=*(cncharlist+start);
+	ch[1]=*(cncharlist+start+1);
+	ch[2]=0;
+	return ch;
+}
+/**
+ * The long Link contain all the chinese character
+ */
+cchar* createLongLink(char* cnchar,char* pinyin,char* py){//TODO
+	int i;
+	cchar* head=(cchar*)malloc(sizeof(cchar));
+	//head->cnch={cnchar[i],cnchar[i+1]};
+	//printf(cnchar);
+	return head;
+}
+/**
+ * main function
+ * program entrance
+ */
 int main(){
-	printf(cnchar);
-	printf("\n%d",sizeof(cnchar));
+	char* ch=cutCnChar(cnchar,2);
+	printf("%s",ch);
+	createLongLink(cnchar,pinyin,py);
 	return 0;
 }
 
