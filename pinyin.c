3eb0446cfcd87f1f1ba1893e8b4a7d9811324a78
rex lee <duguying2008@gmail.com>
2013-08-14 13:12:38 +0800

    fn:cutPinyin

--- pinyin.c
+++ pinyin.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "pinyin.inc"
+#include "pinyin.h"
 
+#define DE '|'
 /**
  * The struct of a single chinese character
  */
@@ -13,9 +15,9 @@ typedef struct character{
 /**
  * cut the chinese character
  * 2013-8-13
- * @param cncharlist the chinese character list
- * @param i the ith chinese character
- * @return the chinese character
+ * @param char*	cncharlist the chinese character list
+ * @param int	i the ith chinese character
+ * @return char*	the chinese character
  */
 char* cutCnChar(char* cncharlist,int i){
 	int length=sizeof(cncharlist)/2;
@@ -26,6 +28,31 @@ char* cutCnChar(char* cncharlist,int i){
 	ch[1]=*(cncharlist+start+1);
 	ch[2]=0;
 	return ch;
+}
+/**
+ * cut the chinese pinyin char
+ * @param char*	pinyin the pinyin charlist
+ * @param int i	the ith pinyin
+ * @return char*	the pinyin
+ */
+char* cutPinyin(char* pinyin,int i){
+	int c=0;//count the separative sign
+	int length=strlen(pinyin);
+	int n;
+	char buf[8]={'\0','\0','\0','\0','\0','\0','\0','\0'};
+	int m=0;//buffer increase
+	for(n=0;n<length;n++){
+		if(DE==pinyin[n]){
+			c++;
+		};
+		if(c==i){
+			if(DE!=pinyin[n]){
+				buf[m]=pinyin[n];
+				m++;
+			}
+		}
+	}
+	printf("%s\n",buf);
 }
 /**
  * The long Link contain all the chinese character
@@ -42,9 +69,12 @@ cchar* createLongLink(char* cnchar,char* pinyin,char* py){//TODO
  * program entrance
  */
 int main(){
-	char* ch=cutCnChar(cnchar,2);
-	printf("%s",ch);
-	createLongLink(cnchar,pinyin,py);
+	//char* ch=cutCnChar(cnchar,2);
+	//printf("%s",ch);
+	//createLongLink(cnchar,pinyin,py);
+	cutPinyin(pinyin,0);
+	cutPinyin(pinyin,1);
+	cutPinyin(pinyin,2);
 	return 0;
 }
 
