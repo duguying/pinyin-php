b96f6678dae3d60a9efdaba46fd34aee8c0127b0
Rex Lee <duguying2008@gmail.com>
2014-04-21 15:29:34 +0800

    Multi-Chinese character support

--- py_pinyin.c
+++ py_pinyin.c
@@ -51,10 +51,61 @@ i_HashTable* pinyin_init(){
  * @return pinyin string
  */
 char* pinyin_get(char* cn){
-	char cnc[3];
-	memset(cnc,0,sizeof(char)*3);
-	strncpy(cnc,cn,2);
-	return (char*)ht_lookup(&dict, cn)->nValue;
+	char single_cn_buf[3];
+	unsigned int len=0;
+	unsigned int len_pybuf=0;
+	unsigned int flag_cnchar_buf=0;
+	char* pybuf=NULL;
+	char* pybuf_pos=NULL;
+	unsigned int i=0;
+
+	len=strlen(cn);
+	len_pybuf=(len/2)*7+1;
+	pybuf_pos=pybuf=(char*)malloc(len_pybuf);
+	memset(pybuf,0,len_pybuf);
+	memset(single_cn_buf,0,3);
+
+	// strip ascii
+	for (i = 0; i < len; ++i)
+	{
+		if(cn[i]<0){ // chinese char
+			if(0==flag_cnchar_buf){
+				single_cn_buf[0]=cn[i];
+				flag_cnchar_buf=1;
+				
+				//printf("0[%d] put into buff\n", cn[i]);
+			}else{
+				char* single_cn_pinyin=NULL;
+				unsigned int len_of_py=0;
+				HashNode* result_node=NULL;
+				
+				//printf("1[%d] put into buff\n", cn[i]);
+				
+				single_cn_buf[1]=cn[i];
+				flag_cnchar_buf=0;
+				// translate
+				
+				result_node=(HashNode*)ht_lookup(&dict, single_cn_buf);
+				
+				if(NULL==result_node){
+					len_of_py=strlen(single_cn_buf);
+					strncpy(pybuf_pos,single_cn_buf,len_of_py);
+					pybuf_pos+=len_of_py;
+				}else{
+					single_cn_pinyin=result_node->nValue;
+					len_of_py=strlen(single_cn_pinyin);
+					strncpy(pybuf_pos,single_cn_pinyin,len_of_py);
+					pybuf_pos+=len_of_py;
+				}
+			}
+		}else{ // other
+			//printf("~[%d] put into buff\n", cn[i]);
+			memset(pybuf_pos,cn[i],1);
+			pybuf_pos+=1;
+		}
+	}
+
+	return pybuf;
 }
 
 /**
--- test_pinyin.c
+++ test_pinyin.c
@@ -3,10 +3,20 @@
 
 int main(int argc, char const *argv[])
 {
-	char* rst;
+	char* rst;int i=0;char* source="わたしわ李俊, and my English name is Rex Lee. 网名是独孤影！ ^_^";
 	
 	pinyin_init();
-	rst=pinyin_get("李");
-	printf("result:%s", rst);
+
+	/*for (i = 0; i < 2; ++i)
+	{
+		if (source[i]<0)
+		{
+			printf("%d\n", source[i]);
+		}
+	}*/
+	
+	rst=pinyin_get(source);
+	printf("%s\n%s", source, rst);
+
 	return 0;
 }
