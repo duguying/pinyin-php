43873231ebba47384fdcf37be3da7dadc595966d
Rex Lee(李俊) <duguying2008@gmail.com>
2015-01-27 17:07:55 +0800

    test model

--- src/test_pinyin.c
+++ src/test_pinyin.c
@@ -4,24 +4,11 @@
 int main(int argc, char const *argv[])
 {
 	char* rst;int i=0;char* source="わたしわ李俊, and my English name is Rex Lee. 网名是独孤影！ ^_^";
-	//HashNode* result_node;
 	
-	pinyin_init();
+    PinTable dict;
+	pinyin_init(&dict);
 
-	/*for (i = 0; i < 2; ++i)
-	{
-		if (source[i]<0)
-		{
-			printf("%d\n", source[i]);
-		}
-	}*/
-	
-	//source="Ò»";
-	//result_node=(HashNode*)ht_lookup(&dict, source);
-	//rst=(char*)result_node->pNext->nValue;
-	
-	rst=pinyin_get(source);
-	printf("%s\n%s", source, rst);
+    ht_print(&dict);
 
 	return 0;
 }
