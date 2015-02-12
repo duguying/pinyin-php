a217e96c19302ca7b60bc29aa7d556e1d0463269
Rex Lee(李俊) <duguying2008@gmail.com>
2015-02-12 21:49:40 +0800

    add open file etc.

--- src/py_pinyin.c
+++ src/py_pinyin.c
@@ -8,6 +8,79 @@ char cnchar[] = "汉";
 char pinyin[] = "hello";
 
 /**
+ * load char dictionary
+ */
+void load_char(const char* filename){
+	long length = file_size_count(filename);
+	char* content = file_open(filename);
+
+	free_buffer(content);
+}
+
+/**
+ * load words dictionary
+ */
+void load_word(const char* filename){
+	long length = file_size_count(filename);
+	char content = file_open(filename);
+
+	free_buffer(content);
+}
+
+/**
+ * get the size of text file
+ * @param filename
+ * @return size of file
+ */
+long* file_size_count(const char* filename){
+	FILE* fp;
+	fp = fopen(filename, "rb");
+	fseek(fp, 0, SEEK_SET);
+	fseek(fp, 0, SEEK_END);
+	long long_bytes = ftell(fp);
+	return long_bytes;
+}
+
+/**
+ * open an file and get content into buffer
+ * @param filename
+ * @return file length
+ */
+char* file_open(const char* filename){
+	char *buffer = NULL;
+	int string_size,read_size;
+	FILE *handler = fopen(filename,"r");
+
+	if (handler){
+		fseek(handler,0,SEEK_END);
+		string_size = ftell (handler);
+		rewind(handler);
+
+		buffer = (char*) malloc (sizeof(char) * (string_size + 1) );
+		read_size = fread(buffer,sizeof(char),string_size,handler);
+		buffer[string_size+1] = '\0';
+
+		if (string_size != read_size) {
+		   free(buffer);
+		   buffer = NULL;
+		}
+	}
+  
+    return buffer;
+}
+
+/**
+ * free the string buffer
+ * @param buffer pointer
+ * @return
+ */
+int free_buffer(char* buffer){
+	free(buffer);
+	buffer = NULL;
+	return 0;
+}
+
+/**
  * initialize pinyin
  * @return
  */
